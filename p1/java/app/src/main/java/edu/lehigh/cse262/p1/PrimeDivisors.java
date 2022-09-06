01399af5d74a16472f086e3436e9fe66cba1957d
wjz224 <wjz224@lehigh.edu>
2022-09-06 19:26:44 -0400

    PrimeDivisors.java computeDivisors() function implemented

--- p1/java/app/src/main/java/edu/lehigh/cse262/p1/PrimeDivisors.java
+++ p1/java/app/src/main/java/edu/lehigh/cse262/p1/PrimeDivisors.java
@@ -1,5 +1,6 @@
 package edu.lehigh.cse262.p1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /** PrimeDivisors is a wrapper class around the function `computeDivisors` */
@@ -12,6 +13,18 @@ public class PrimeDivisors {
    */
   List<Integer> computeDivisors(int value) {
     // [CSE 262] Implement Me!
-    return null;
+    List<Integer> list = new ArrayList<>();
+    // iterates up to value and checks for prime divisors
+    for(int i = 2; i < value; i++){
+      // 
+      while(value%i == 0){
+        list.add(i);
+        value = value/i;
+      }
+    }
+    if(value > 2){
+      list.add(value);
+    }
+    return list;
   }
 }
