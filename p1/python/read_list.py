c557a7b0479aa98973e0013a944d9c011b042fd3
wjz224 <wjz224@lehigh.edu>
2022-09-07 16:42:22 -0400

    read_list.py implemented

--- p1/python/read_list.py
+++ p1/python/read_list.py
@@ -5,4 +5,21 @@
 # the order in which they were entered.
 
 def read_list():
-    pass
+    #try recursive function until EOF
+    try:
+        # empty list
+        list = []
+        # Tell the user to enter an element to the list
+        print("Enter to add element to list:")
+        # Append the users input into the list
+        list.append(input())
+        # Extend the list with additional user inputs by recursive call on read_list function until EOF
+        list.extend(read_list())
+        # return list for extension and returns entire list when all user inputs are added into list
+        return list
+    # except catches EOF
+    except:
+        # Once EOF is reached,the recursive function finally stops and the last element is added
+        return list
+# test
+# print(read_list())
