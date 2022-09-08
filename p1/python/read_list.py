3e38590c264bb98575fe127e60bf4fe7ebc3a658
wjz224 <wjz224@lehigh.edu>
2022-09-08 18:12:49 -0400

    read_list.py changed variable name because list is a python built-in type to avoid confusion

--- p1/python/read_list.py
+++ p1/python/read_list.py
@@ -8,18 +8,18 @@ def read_list():
     #try recursive function until EOF
     try:
         # empty list
-        list = []
+        ls = []
         # Tell the user to enter an element to the list
         print("Enter to add element to list:")
         # Ask user for input and store it in variable x
         x = input()
         # Extend the list with additional user inputs by recursive call on read_list function until EOF
-        list.extend(read_list())
+        ls.extend(read_list())
          # Append the users input into the list in reverse order from which they were entered
-        list.append(x)
+        ls.append(x)
         # return list for extension and returns the reversed list of all the inputs the user entered
-        return list
+        return ls
     # except catches EOF
     except:
         # Once EOF is reached,the recursive calls finally stop and the functions start returning after the last element is added
-        return list
+        return ls
