2845f99f1b50142d9d6938118965a4343e4d4d76
wjz224 <wjz224@lehigh.edu>
2022-09-08 15:05:58 -0400

    my_map.py added None case and added comments

--- p1/python/my_map.py
+++ p1/python/my_map.py
@@ -5,6 +5,12 @@
 # `map` function.
 
 def my_map(func, l):
+    # if list is none return none
+    if(l == None):
+        return None
+    # For loop applies the function across all elements in the list l
     for i in range(len(l)):
+        # Setting the element to value returned from the function
         l[i] = func(l[i])
+    # return list after for loop
     return l
