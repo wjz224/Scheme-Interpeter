d28fa3dd75a5242c251700127ee8be4d0db4f54a
wjz224 <wjz224@lehigh.edu>
2022-09-08 12:57:50 -0400

    my_reverse.py() comment changes

--- p1/python/my_reverse.py
+++ p1/python/my_reverse.py
@@ -8,8 +8,9 @@ def my_reverse(l):
     if(len(l) < 2):
         return l
     else:
-        # return the list's last element and then add the  recursive call on the the list discluding the last element 
-        # the final return would the reversed list
+        # return the list's last element plus the recursive call on the the list discluding the last element.
+        # This recursive call continues until we reach the first index of the original list
+        # the final return would be the reversed list
         return [l[-1]] + my_reverse(l[:-1])
 # testing
 #l = [1,2,3,4,5,6]
