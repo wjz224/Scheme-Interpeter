6792806d09e86e2392745aab41c5be6c81b3b412
wjz224 <wjz224@lehigh.edu>
2022-09-08 19:49:42 -0400

    my_reverse.scm() implemented

--- p1/scheme/my_reverse.scm
+++ p1/scheme/my_reverse.scm
@@ -6,6 +6,16 @@
 ;;
 ;; Your implementation should be recursive.
 
-(define (my-reverse l)
-  #f ;; [CSE 262] Implement Me!
-  )
+(define (reverse l)
+  ;; if statement here checks if list l is null
+  (if (null? l)
+    ;; if true return l
+     l 
+    ;; if false append a recursive call of all the elements after the first element in a list using 'cdr'
+    ;; then grab the very first element of the list using 'car' (this gets appended to the very end of a new list because of the recursive call used for 'cdr' l)
+     (append (reverse (cdr l)) 
+             (list (car l)))))
+;; testing             
+;;(define a (list 1 2 3 4))
+;;(display (reverse a))
+
