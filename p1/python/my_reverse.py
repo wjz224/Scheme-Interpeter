6682c8d07b64936d2b4c33ae6c5f194f2ec6d0c7
wjz224 <wjz224@lehigh.edu>
2022-09-08 02:28:38 -0400

    my_reverse.py implemented

--- p1/python/my_reverse.py
+++ p1/python/my_reverse.py
@@ -1,10 +1,18 @@
 # my_reverse: reverse a list without using the python `reverse` function
 
 def my_reverse(l):
-    if (l == none):
-        return none
-    if (l.size() < 2):
+    # if list is none than it will return none
+    if(l == None):
+        return None
+    # if list is empty or just one element, return list
+    if(len(l) < 2):
         return l
-    else: 
-        x = 
-    pass
+    else:
+        # return the list's last element and then add the  recursive call on the the list discluding the last element 
+        # the final return would the reversed list
+        return [l[-1]] + my_reverse(l[:-1])
+# testing
+#l = [1,2,3,4,5,6]
+#print(my_reverse(l))
+
+
