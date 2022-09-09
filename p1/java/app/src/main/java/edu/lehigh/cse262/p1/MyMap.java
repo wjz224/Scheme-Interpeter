3c68e724c3ea4dfdbf550b3b9cce91f7aa055c5f
wjz224 <wjz224@lehigh.edu>
2022-09-09 15:25:43 -0400

    Mymap.java comments

--- p1/java/app/src/main/java/edu/lehigh/cse262/p1/MyMap.java
+++ p1/java/app/src/main/java/edu/lehigh/cse262/p1/MyMap.java
@@ -16,13 +16,18 @@ public class MyMap<T> {
    */
   List<T> map(List<T> list, Function<T, T> func) {
     // [CSE 262] Implement Me!
+    //if list is null then return null
     if(list == null){
       return null;
     }
+    //create a new ArrayList of type List<T> called output
     List<T> output = new ArrayList<>();
+    //iterate from 0 to list.size()
     for(int i = 0; i < list.size(); i++){
+        //apply func to each element of list and adding it to output
         output.add(func.apply(list.get(i)));
     }
+    //return output
     return output;
   }
 }
