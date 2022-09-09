51d584f5cb1b2854b331d7788580b8e55c1c0561
wjz224 <wjz224@lehigh.edu>
2022-09-08 23:17:36 -0400

    tree.py fixed insertion scope issue and added clear()

--- p1/python/tree.py
+++ p1/python/tree.py
@@ -13,7 +13,7 @@
 # inner node class
 class node:
     # node initalize. Starts a node with x
-    def _init_(self, x):
+    def __init__(self, x):
         # setting the node's value with x
         self.value = x
         # setting the node's left child as None
@@ -22,7 +22,7 @@ class node:
         self.right = None
 class tree:
     # tree initalizer
-    def _init_(self):
+    def __init__(self):
         # start tree root as none  
         self.root = None
     # tree ins(x) method - inserts x into tree
@@ -37,54 +37,110 @@ class tree:
         # If there is a root, traverse through tree starting from root using recursion to insert x
         else:
             # calling recursive helper function to insert x into the tree starting from root
-            self.insertHelper(x,self.root)
+            self.insHelper(x,self.root)
     # recursive ins helper function that traverses through tree and inserts x as a node in the tree
     def insHelper(self, x, current):
-        # if current is None than we are at the spot where the node should be inserted
-        if(current == None):
-            # setting current to the newly created node with value x
-            current = node(x)
-        # if current is not None, keep traversing 
-        else:
-            # if x is less than current value than go to left child
-            if(x < current.value):
+        # if x is less than current value  than go to left child
+        if(x < current.value):
+            # if left child is none than set left child to new node with value x object.
+            # I have to set current.left and can't just traverse and set current = node(x) when current == none because of the scope.
+            if(current.left == None):
+                # setting current left child to new node object with value x
+                current.left = node(x)
+            # else call the recursive helper method on the left child
+            # I have to set current.left and can't just traverse and set current = node(x) when current == none because of the scope.
+            else:   
                 # calling recursive helper method on left child
-                self.insHelper(current.left)
-            # if x is greater than current value than go to right child
-            elif (x > current.value):
-                # calling recursive method on right child
-                self.insHelper(current.right)
+                self.insHelper(x,current.left)
+            # if x is greater than or equal to (equal to accoutns for duplicates) current value than go to right child
+        elif (x >= current.value):
+            # if right child is none than set right child to new node with value x object
+            if(current.right == None):
+            # setting current right child to new node object with value x
+                current.right = node(x)
             else:
-                # return since there the value is a duplicate
-                return
+            # calling recursive method on right child
+                self.insHelper(x,current.right) 
     # tree inslist(l) method - inserts list into tree
     def inslist(self,l):
         # don't insert if list is none, return
         if(l == None):
             return
         # for loop that inserts each element from the list into the tree
-        for i in l:
+        for i in range(len(l)):
             # inserting each element from the list
-            self.insert(i)
+            self.ins(l[i])
+    # tree clear() method - clears method by setting root to None
+    def clear(self):
+        #setting root to none
+        self.root = None
     # tree display() method - display the tree in preorder
     def display(self):
         # if root is None than there is nothing to display
         if(self.root == None):
+            # if nothing show an empty list
+            # print bracket for style
+            print("[", end =" ")
+            # #print bracket for style
+            print("]", end =" ")
             return
-        # calling recursive helper function starting from root
-    # recursive display helper function that displays the tree in pre order
-    def displayHelper(self, current):
+        # print bracket for style 
+        print("[", end =" ")
+        # calling recursive display helper function starting from root
+        self.displayHelper(self.root)
+        print("]", end =" ")
+    # recursive display helper function that displays the tree in in order
+    def displayHelper(self, current): 
         if (current != None):
             # visit the node and print the current's value
-            print(current.value + " ")
-            # Traverse the left tree by recursive call on the left child
             self.displayHelper(current.left)
+            # Traverse the left tree by recursive call on the left child
+            print(current.value, end = " ")
             # Traverse the right tree by recursive call on the right child
             self.displayHelper(current.right)
-    def inorder(self):
+    def inorder(self, func):
         # if root is None than there is nothing to traverse
         if(self.root == None):
             return
-        self.inorderHelper()
-    def inorderHelper(self,current):
-        
+        # calling recursive inorder helper function that traverses the tree in order and applies the func function
+        self.inorderHelper(self.root,func)
+    def inorderHelper(self,current,func):
+        if(current != None):
+            # traverse through left tree by recursively calling the helper function on the left child
+            self.inorderHelper(current.left,func) 
+            # Once traversed through left tree, visit node and apply func 
+            current.value = func(current.value)
+            # traverse through right tree by recursively calling the helper function on the right child
+            self.inorderHelper(current.right,func)
+    def preorder(self, func):
+        # if root is None than there is nothing to traverse
+        if(self.root == None):
+            return
+        # calling recursive inorder helper function that traverses the tree in order and applies the func function
+        self.preorderHelper(self.root,func)
+    def preorderHelper(self,current,func):
+        if(current != None):
+            # traverse through left tree by recursively calling the helper function on the left child
+            self.preorderHelper(current.left,func)
+            # Once traversed through left tree, visit node and apply func
+            current.value = func(current.value)
+            # traverse through right tree by recursively calling the helper function on the right child
+            self.preorderHelper(current.right,func)
+#testing
+#def add2(a):
+       # a += 2
+        #return a
+
+#o = tree()
+#o.ins(2)
+#list2 = [1,2,3,4,5,6]
+#o.ins(2)
+#o.inslist(list2)
+#o.display()
+#o.preorder(add2)
+#o.display()
+#o.inorder(add2)
+#o.display()
+#o.clear()
+#o.display()
+#print()
