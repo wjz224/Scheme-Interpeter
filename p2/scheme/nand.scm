959f71eb70ed1978d3173b57e6fa303129088685
wjz224 <wjz224@lehigh.edu>
2022-10-08 17:38:42 -0400

    p2 nand.scm finished

--- p2/scheme/nand.scm
+++ p2/scheme/nand.scm
@@ -1,4 +1,7 @@
 ;; Compute the nand of two values, using only the `or` and `not` functions
 ;;
 ;; nand should always return a boolean value
-(define (nand a b) #f)
+
+(define (nand a b)
+    (not (and a b)
+)
