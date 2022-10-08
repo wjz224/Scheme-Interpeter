3dfbd909a08808b613b0244e627277db4d50145a
wjz224 <wjz224@lehigh.edu>
2022-10-08 17:59:09 -0400

    p2 xor.scm finished

--- p2/scheme/xor.scm
+++ p2/scheme/xor.scm
@@ -1,4 +1,10 @@
 ;; Compute the exclusive or of two values, using only and, or, and not
 ;;
 ;; xor should always return a boolean value
-(define (xor a b) #f)
+
+(define (xor a b) 
+    (and (not (and a b)) (or a b))
+)
+;; testing(
+;; define a (xor #t #t))
+;;(display a)
