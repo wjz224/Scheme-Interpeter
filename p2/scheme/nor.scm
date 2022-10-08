5c5b51872ba6b1cad336f62852f5f9f68853950f
wjz224 <wjz224@lehigh.edu>
2022-10-08 17:39:07 -0400

    p2 nor.scm finished

--- p2/scheme/nor.scm
+++ p2/scheme/nor.scm
@@ -1,4 +1,11 @@
 ;; Compute the nor of two values, using only the `and` and `not` functions
 ;;
 ;; nor should always return a boolean value
-(define (nor a b) #f)
+
+(define (nor a b)
+ (and (not a) (not b))
+)
+
+;; testing
+;;(define a (nor #t #t))
+;;(display a)
