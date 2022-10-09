acba3998ed1691e4ed6532f8c2e583a204d93abe
wjz224 <wjz224@lehigh.edu>
2022-10-09 19:06:04 -0400

    p2 reduce.scm finished and comments

--- p2/scheme/reduce.scm
+++ p2/scheme/reduce.scm
@@ -4,4 +4,18 @@
 ;; Example: (reduce + '(1 2 3) 0) ==> 6
 ;;
 ;; Example: (reduce * '() 1) ==> 1
-(define (reduce op l identity) '())
+(define (reduce op l identity) 
+     ;; check if list is null
+    (if (null? l)
+        ;; if list is null than just return the identity
+        identity
+        ;; if list is not null than apply operator on the first element of the list l
+        ;; than call the recursive function on the remaining elements of the list
+        ;; the remaining elements is the cdr of the list l.
+        ;; this continues until the list is cut down into null.
+        (op (car l)
+            (reduce op (cdr l) identity))))
+;;testing
+
+;;(define a (reduce + '(1 2 3) 0))
+;;(display a)
