eb4dc8ac2afc1601b46c215ea21df9580cb4b31e
wjz224 <wjz224@lehigh.edu>
2022-10-09 02:47:50 -0400

    l2v.scm finished

--- p2/scheme/l2v.scm
+++ p2/scheme/l2v.scm
@@ -1,2 +1,17 @@
 ;; list2vector takes a list and returns a vector, without using `list->vector`
-(define (list2vector vec) '())
+(define (list2vector vec) 
+  (letrec ((helper (lambda ( lstvec)
+     (let ((listCheck (not(= (length lstvec) 1))))
+     (cond 
+        (listCheck (vector-append (vector (car lstvec)) (list2vector (cdr lstvec))))
+        (else (vector (car lstvec)))
+     )
+     
+     ))))
+        (helper vec)
+  )
+)
+
+
+(define a (list2vector (list 1 2 3 4)))
+(display a)
