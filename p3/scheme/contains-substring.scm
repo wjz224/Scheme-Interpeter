10a10a96296691f52fb2093ada847e1224bb4225
wjz224 <wjz224@lehigh.edu>
2022-11-13 23:37:00 -0500

    scheme contains-substring.scm finished

--- p3/scheme/contains-substring.scm
+++ p3/scheme/contains-substring.scm
@@ -12,5 +12,30 @@
 ;; You should implement this on your own, by comparing one character at a time,
 ;; and should not use any string comparison functions that are provided by gsi.
 
-;; TODO: implement this function
-(define (contains-substring source pattern) '())
+;; string-contains function that checks if substring is in str
+(define (contains-substring source pattern)
+  (let ((n (- (string-length source) 1)) (n2 (string-length pattern))) 
+    (and 
+        ;;loop that keeps track of index of str and sub, i is for the character index of sub and count is for the character of str
+         (let loop ((i 0) (count 0))
+            ;;conditional that evaulates to true when i is equal to length of sub (that means we found all characters of sub in a row in str)
+            (cond ((= i n2) #t) 
+                  ;; if char at index count matches character at index sub than recursively call loop with incremented i and count
+                  ((char=? (string-ref source count) (string-ref pattern i)) (loop (+ i 1) (+ count 1)))
+                  ;;when count is not at the end of the string, but the characters at count and i doesn't match, recursively call the loop with a resetted i.
+                  ((not (= count n)) 
+                    ;;conditional when substring index is greater than 0 we reset the substring index and keep the string index to check if the current index is the start of the substring
+                    (cond ((> i 0) (loop 0 count))
+                          ;;conditional when substring index is == 0 so we keep the substring index and increment the string index
+                          ((= i 0) (loop 0 (+ count 1)))
+                    ))
+                  ;;return false when count is equal to str length and i is not equal to the length of the substring sub
+                  ((= count n) #f)
+            )   
+         )
+    )
+  )
+)
+
+;;testing
+(display (contains-substring "hello" "lo"))
