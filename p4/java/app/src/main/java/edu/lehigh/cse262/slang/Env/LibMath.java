59c8d1fc6537ac03104faab4d2946780c5b9ed4f
wjz224 <wjz224@lehigh.edu>
2022-11-27 20:53:04 -0500

    some functions added for mathlib

--- p4/java/app/src/main/java/edu/lehigh/cse262/slang/Env/LibMath.java
+++ p4/java/app/src/main/java/edu/lehigh/cse262/slang/Env/LibMath.java
@@ -22,7 +22,8 @@ public class LibMath {
         // Note that this code is **very** tedious. Making some helper
         // functions would probably be wise, but it's up to you to figure out
         // how.
-        var add = new Nodes.BuiltInFunc("+", (List<IValue> args) -> {
+        // built in func for addition
+        var add = new Nodes.BuiltInFunc("+", (List<IValue> args) -> { // (+ 1 2 3 4)
             // Type checking: make sure we only have int and dbl arguments. We also will use
             // this to know if we should be returning an Int or a Dbl
             int intCount = 0;
@@ -56,8 +57,195 @@ public class LibMath {
                 return new Nodes.Int(result);
             }
         });
-
         map.put(add.name, add);
+        var subtract = new Nodes.BuiltInFunc("-", (List<IValue> args) -> { // (+ 1 2 3 4)
+            // Type checking: make sure we only have int and dbl arguments. We also will use
+            // this to know if we should be returning an Int or a Dbl
+            int intCount = 0;
+            int dblCount = 0;
+            for (var arg : args) {
+                if (arg instanceof Nodes.Int)
+                    intCount++;
+                if (arg instanceof Nodes.Dbl)
+                    dblCount++;
+            }
+            if (args.size() > (intCount + dblCount))
+                throw new Exception("/ can only handle Int and Dbl arguments");
+            // Semantic analysis: make sure there are arguments!
+            if (args.size() == 0)
+                throw new Exception("/ expects at least one argument");
+            // Compute, making sure to know the return type
+            if (dblCount > 0) {
+                double result = ((Nodes.Dbl)args[0]).val;
+                for (int i= 1; i < args.size(); i++) {
+                    if (args[i] instanceof Nodes.Int)
+                        result -= ((Nodes.Int) args[i]).val;
+                    else
+                        result -= ((Nodes.Dbl) args[i]).val;
+                }
+                return new Nodes.Dbl(result);
+            } else {
+                int result = ((Nodes.Int)args[0]).val;
+                for (int i = 1; i < args.size(); i++) {
+                    result -= ((Nodes.Int) args[i]).val;
+                }
+                return new Nodes.Int(result);
+            }
+        });
 
+        map.put(subtract.name, subtract);
+        
+        var multiplication = new Nodes.BuiltInFunc("*", (List<IValue> args) -> { // (+ 1 2 3 4)
+            // Type checking: make sure we only have int and dbl arguments. We also will use
+            // this to know if we should be returning an Int or a Dbl
+            int intCount = 0;
+            int dblCount = 0;
+            for (var arg : args) {
+                if (arg instanceof Nodes.Int)
+                    intCount++;
+                if (arg instanceof Nodes.Dbl)
+                    dblCount++;
+            }
+            if (args.size() > (intCount + dblCount))
+                throw new Exception("* can only handle Int and Dbl arguments");
+            // Semantic analysis: make sure there are arguments!
+            if (args.size() == 0)
+                throw new Exception("* expects at least one argument");
+            // Compute, making sure to know the return type
+            if (dblCount > 0) {
+                double result = 0;
+                for (var arg : args) {
+                    if (arg instanceof Nodes.Int)
+                        result *= ((Nodes.Int) arg).val;
+                    else
+                        result *= ((Nodes.Dbl) arg).val;
+                }
+                return new Nodes.Dbl(result);
+            } else {
+                int result = 0;
+                for (var arg : args) {
+                    result *= ((Nodes.Int) arg).val;
+                }
+                return new Nodes.Int(result);
+            }
+        });
+        map.put(multiplication.name, multiplication);
+        // built in func for divison
+    var division = new Nodes.BuiltInFunc("/", (List<IValue> args) -> { // (+ 1 2 3 4)
+            // Type checking: make sure we only have int and dbl arguments. We also will use
+            // this to know if we should be returning an Int or a Dbl
+            int intCount = 0;
+            int dblCount = 0;
+            for (var arg : args) {
+                if (arg instanceof Nodes.Int)
+                    intCount++;
+                if (arg instanceof Nodes.Dbl)
+                    dblCount++;
+            }
+            if (args.size() > (intCount + dblCount))
+                throw new Exception("/ can only handle Int and Dbl arguments");
+            // Semantic analysis: make sure there are arguments!
+            if (args.size() == 0)
+                throw new Exception("/ expects at least one argument");
+            // Compute, making sure to know the return type
+            if (dblCount > 0) {
+                double result = ((Nodes.Dbl)args[0]).val;
+                for (int i= 1; i < args.size(); i++) {
+                    if (args[i] instanceof Nodes.Int)
+                        result /= ((Nodes.Int) args[i]).val;
+                    else
+                        result /= ((Nodes.Dbl) args[i]).val;
+                }
+                return new Nodes.Dbl(result);
+            } else {
+                int result = ((Nodes.Int)args[0]).val;
+                for (int i = 1; i < args.size(); i++) {
+                    result  /= ((Nodes.Int) args[i]).val;
+                }
+                return new Nodes.Int(result);
+            }
+        });
+        map.put(division.name, division);
+    
+    // built in func for divison
+    var modulo = new Nodes.BuiltInFunc("%", (List<IValue> args) -> { // (+ 1 2 3 4)
+        // Type checking: make sure we only have int and dbl arguments. We also will use
+        // this to know if we should be returning an Int or a Dbl
+        if(arg.size > 2 || arg.size == 1){
+            throw new Exception ("Wrong number of arguments passed into procedure")
+        }
+        int intCount = 0;
+        for (var arg : args) {
+            if (arg instanceof Nodes.Int)
+                intCount++;
+        }
+        if (args.size() > (intCount))
+            throw new Exception("% can only handle Int arguments");
+        // Semantic analysis: make sure there are arguments!
+        if (args.size() == 0)
+            throw new Exception("% expects two argument");
+        // Compute, making sure to know the return type
+        if (intCount > 0) {
+            int num1 = ((Nodes.Int)args[0]).val;
+            int num2 = ((Nodes.Int)args[1]).val;
+            int result = ((Nodes.Int) (num % num2)).val;
+            return new Nodes.Int(result);
+        } 
+    });
+    map.put(modulo.name, modulo);
+
+    var modulo = new Nodes.BuiltInFunc("==", (List<IValue> args) -> { // (+ 1 2 3 4)
+        // Type checking: make sure we only have int and dbl arguments. We also will use
+        // this to know if we should be returning an Int or a Dbl
+        if(arg.size > 2 || arg.size == 1){
+            throw new Exception ("Wrong number of arguments passed into procedure")
+        }
+        int intCount = 0;
+        int dblCount = 0;
+        int boolCount = 0;
+        for (var arg : args) {
+            if (arg instanceof Nodes.Int)
+                intCount++;
+            if (arg instanceof Nodes.Dbl)
+                dblCount++;
+            if (arg instanceof Node.Bool)
+                boolCount++;
+        }
+        if (args.size() > (intCount + dblCount + boolCount))
+            throw new Exception("== can only handle Int, Dbl, and Bool arguments");
+        // Semantic analysis: make sure there are arguments!
+        if (args.size() == 0)
+            throw new Exception("== expects two arguments");
+        // Compute, making sure to know the return type
+        var num1 = 0;
+        var num2 = 0
+        if (args[0] instanceof Nodes.Int){
+            num1 =  ((Nodes.Int) args[0]).val;
+        }
+        else if (args[0] instanceof Nodes.Dbl){
+            num1 =  ((Nodes.Dbl) args[0]).val;
+        }
+        else{
+            num1 = ((Nodes.Bool) args[0].val);
+        }
+        if (args[1] instanceof Nodes.Int){
+            num2 =  ((Nodes.Int) args[1]).val;
+        }
+        else if (args[1] instanceof Nodes.Dbl){
+            num2 =  ((Nodes.Dbl) args[1]).val;
+        }
+        else{
+            num2 = ((Nodes.Bool) args[1].val);
+        }
+        if(num1 == num2){
+            return poundT;
+        }
+        else{
+            return poundF
+        }
+            
+            
+    });
+    map.put(modulo.name, modulo);
     }
 }
